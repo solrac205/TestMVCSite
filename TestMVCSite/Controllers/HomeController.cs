161ff915e60b915fef3a83d1a96ce3c7c817067d
Carlos Ramírez <carlos_205ram@hotmail.com>
2021-06-03 19:33:25 -0600

    Changes to connect to web API.
    not connect, API generate error forbidden 403

--- TestMVCSite/Controllers/HomeController.cs
+++ TestMVCSite/Controllers/HomeController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 using TestMVCSite.Models;
 
 namespace TestMVCSite.Controllers
@@ -28,7 +30,37 @@ namespace TestMVCSite.Controllers
 
             return View();
         }
-
+        public async Task<IActionResult> CovidReport()
+        {
+            ViewData["Message"] = "COVID Report";
+            
+            /*var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://covid-19-statistics.p.rapidapi.com/regions"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "SIGN-UP-FOR-KEY" },
+                    { "x-rapidapi-host", "covid-19-statistics.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+                return View(body);
+            }*/
+            
+            var client = new RestClient("https://covid-19-statistics.p.rapidapi.com/regions");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-key", "SIGN-UP-FOR-KEY");
+            request.AddHeader("x-rapidapi-host", "covid-19-statistics.p.rapidapi.com");
+            var response = client.Execute(request);
+            
+            return View(response);
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
